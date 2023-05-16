928e0986d201c1ddc16c05a0f816b38526f24a53
keelieflowers <keelie.flowers34@gmail.com>
2023-05-16 11:15:05 -0400

    add graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplified": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
